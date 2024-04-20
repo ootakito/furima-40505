4a8a8db4965ffb97e1040e4d4fa68d7570ff9780
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-21 08:14:42 +0900

    モデル関連が一応完成した

---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+  has_one_attached :image
+  おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+  validates :image, presence: true
+  validates :name, :description, presence: true
+  validates :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id,
+            :shipping_duration_id, :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id, :shipping_duration_id,
+            numericality: { other_than: 0 }
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost_burden
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_duration
+end
