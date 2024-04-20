2d0d012968b0ec7c76069a939bb1fcba69e654ee
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-21 08:47:44 +0900

    なぜかボタン連打するバグが発生して修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+  has_one :order, dependent: :destroy
   has_one_attached :image
-  おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお
+
   validates :image, presence: true
   validates :name, :description, presence: true
   validates :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id,
             :shipping_duration_id, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id, :shipping_duration_id,
-            numericality: { other_than: 0 }
+            numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
