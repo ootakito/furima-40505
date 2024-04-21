9d16891354fb9b8dbf7dff9ea7897fb06482187a
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-21 09:27:07 +0900

    ルーティングとコントローラが一応雛形が出来た

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,57 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :redirect_unless_owner, only: [:edit, :update, :destroy]
+
   def index
+    @items = Item.all
+  end
+
+  def show
+    # コメントやその他の関連情報もここでロードする場合
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :category_id, :condition_id, :shipping_cost_burden_id,
+                                 :prefecture_id, :shipping_duration_id, :price)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    redirect_to root_path unless current_user == @item.user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Deviseのルート
-  resources :items, only: [:index]
-  # トップページへのルート
-  root to: "items#index"
+  root 'items#index'
+  resources :items
 end
