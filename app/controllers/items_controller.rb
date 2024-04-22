56762ff0e48859fc2a7635af054c29033a7eb65b
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-22 20:39:24 +0900

    オーダーメソッド追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create] # ユーザーがログインしていることを保証
 
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
