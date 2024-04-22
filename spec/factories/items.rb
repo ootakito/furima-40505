3e81cba691556f3717f4cd69a8c607c61d29c49f
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-22 16:06:15 +0900

    仮でテストコード出来た

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,8 +7,27 @@ FactoryBot.define do
     shipping_cost_burden_id { 2 }
     prefecture_id         { 2 }
     shipping_duration_id  { 2 }
-    price                 { 500 }
-    user                  { association :user }
+    association :user
+
+    transient do
+      price { 500 }
+    end
+
+    after(:build) do |item, evaluator|
+      item.price = evaluator.price
+    end
+
+    trait :with_valid_price do
+      price { 300 }
+    end
+
+    trait :with_invalid_low_price do
+      price { 299 }
+    end
+
+    trait :with_invalid_high_price do
+      price { 10_000_000 }
+    end
     # 以下、画像に関する設定例
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item) # テスト用のItemインスタンスを初期化
   end
 
   context '有効な場合' do
@@ -21,10 +22,40 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーが「---」だと登録できない' do
       @item.category_id = 1
       @item.valid?
-      expect(@item.errors[:category_id]).to include("can't be blank")
+      expect(@item.errors[:category_id]).to include('must be other than 1')
     end
 
-    it '価格が10,000,000円以上だと登録できない' do
+    it '商品の状態が「---」だと登録できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors[:condition_id]).to include('must be other than 1')
+    end
+
+    it '送料負担が「---」だと登録できない' do
+      @item.shipping_cost_burden_id = 1
+      @item.valid?
+      expect(@item.errors[:shipping_cost_burden_id]).to include('must be other than 1')
+    end
+
+    it '発送元の地域が「---」だと登録できない' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors[:prefecture_id]).to include('must be other than 1')
+    end
+
+    it '発送までの日数が「---」だと登録できない' do
+      @item.shipping_duration_id = 1
+      @item.valid?
+      expect(@item.errors[:shipping_duration_id]).to include('must be other than 1')
+    end
+
+    it '価格が¥300未満の場合登録できない' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors[:price]).to include('must be greater than or equal to 300')
+    end
+
+    it '価格が¥9,999,999を超える場合登録できない' do
       @item.price = 10_000_000
       @item.valid?
       expect(@item.errors[:price]).to include('must be less than or equal to 9999999')
@@ -37,7 +68,7 @@ RSpec.describe Item, type: :model do
     end
 
     it '画像が添付されていないと登録できない' do
-      @item.image.detach
+      @item.image.detach # 画像をデタッチしてテスト
       @item.valid?
       expect(@item.errors[:image]).to include("can't be blank")
     end
