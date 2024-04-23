e0ef3bab03826efcd35fb77a5942adb64bc8427e
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-23 20:22:03 +0900

    不必要な記載を消しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
