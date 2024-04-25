38ced14f697eaecfcc3c1fd19675926e3898fc01
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-25 15:50:37 +0900

    モデルにおける不要な記述とテストコードの記述を開始

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,9 +7,9 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id
-    validates :city
-    validates :address
+    validates :prefecture_id, presence: true
+    validates :city, presence: true
+    validates :address, presence: true
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     # validates :token # トークンはクレジット決済のために必須
   end
