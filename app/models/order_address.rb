fc6ebaf27d756fd25e3ec923bc98986f838bda63
ootakito <165157559+ootakito@users.noreply.github.com>
2024-04-28 17:53:03 +0900

    モデルの記述ミスの修正とユーザモデルに書き忘れてたアソシエーション追加しました。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,14 +3,14 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :building, :prefecture_id, :city, :address, :phone_number, :token
 
   with_options presence: true do
-    validates :token, presence: true
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' },
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' },
                              length: { minimum: 10, message: 'is too short' }
   end
 
